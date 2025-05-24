f25d77f0344e970ac1be26ef538be89af5e4752e
GiovanniRumma04 <g.rumma3@studenti.unisa.it>
2025-05-24 17:39:27 +0200

    uodate

--- Gestore_spese/lib/View/PagListeView.dart
+++ Gestore_spese/lib/View/PagListeView.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gestore_spese/Model/Categoria.dart';
 import 'package:gestore_spese/Model/GestoreApp.dart';
-import 'package:gestore_spese/Model/ListaSpese.dart';
-import 'package:gestore_spese/Model/Prodotto.dart';
-import 'package:gestore_spese/Model/Spesa.dart';
 import 'package:gestore_spese/View/BloccoLista.dart';
 
 class ListeView extends StatefulWidget {
@@ -12,59 +8,6 @@ class ListeView extends StatefulWidget {
 }
 
 class _listeViewState extends State<ListeView> {
-
-  /*void avvia(){
-    setState(() {
-      ListaSpese l = new ListaSpese('lista1');
-      ListaSpese l1 = new ListaSpese('lista2');
-      ListaSpese l2 = new ListaSpese('lista3');
-      Categoria c1 = new Categoria('cibo');
-      Categoria c2 = new Categoria('elettronica');
-      Prodotto p = new Prodotto('uova', 2.5, c1, 'un pacco');
-
-      Prodotto p1 = new Prodotto('farina', 2.5, c1, 'un pacco');
-
-      Prodotto p2 = new Prodotto('pane', 2.5, c1, 'dsadsad');
-
-      Spesa s1 = new Spesa(p, DateTime.now(), 3);
-      Spesa s2 = new Spesa(p1, DateTime.now(), 5);
-      Spesa s3 = new Spesa(p2, DateTime.now(), 1);
-
-      l.aggiungiSpesa(s1);
-      l.aggiungiSpesa(s2);
-      l.aggiungiSpesa(s3);
-      l.aggiungiSpesa(s1);
-      l.aggiungiSpesa(s3);
-      l.aggiungiSpesa(s2);
-      l.aggiungiSpesa(s1);
-      l.aggiungiSpesa(s2);
-      l.aggiungiSpesa(s3);
-      l.aggiungiSpesa(s1);
-      l.aggiungiSpesa(s3);
-      l.aggiungiSpesa(s2);
-
-      l1.aggiungiSpesa(s1);
-      l1.aggiungiSpesa(s2);
-      l1.aggiungiSpesa(s3);
-
-      l2.aggiungiSpesa(s2);
-      l2.aggiungiSpesa(s1);
-      l2.aggiungiSpesa(s3);
-
-      listaCentrale.add(l);
-      listaCentrale.add(l1);
-      listaCentrale.add(l2);
-      listaCentrale.add(l);
-      listaCentrale.add(l1);
-      listaCentrale.add(l2);
-      listaCentrale.add(l);
-      listaCentrale.add(l1);
-      listaCentrale.add(l2);
-      listaCentrale.add(l);
-      listaCentrale.add(l1);
-      listaCentrale.add(l2);
-    });
-  }*/
 
   @override
   Widget build(BuildContext context) {
